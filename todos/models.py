eedef6830e67eba6bc8ab1cde235d49403ca4e2a
WanderleiSI <Wanderleirochavasco@hotmail.com>
2023-12-14 08:48:19 -0300

    Lista emprestimo - id cliente e id livro

--- todos/models.py
+++ todos/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 from datetime import date
-
+from cliente.models import Cliente
+from livro.models import Livro
 # Create your models here.
 
 
 class Todo(models.Model):
     title = models.CharField(verbose_name="Título", max_length=100, null=False, blank=False)
+    cliente = models.ForeignKey(Cliente, verbose_name="Cpf cliente", on_delete=models.CASCADE)
+    livro = models.ForeignKey(Livro, verbose_name="Livro", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, null=False, blank=False)
     deadline = models.DateField(verbose_name="Data inicial", null=False, blank=False)
     finished = models.DateField(verbose_name="Data final", null=True)
