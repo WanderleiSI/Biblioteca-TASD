4b879f5303a8abb1e23f4da76f1c96e67564dd00
WanderleiSI <Wanderleirochavasco@hotmail.com>
2023-12-20 21:35:27 -0300

    Removendo senha e usuario do envio de e-mail

--- envia_email/views.py
+++ envia_email/views.py
@@ -10,12 +10,12 @@ from todos.models import Todo
 class EnvEmailView(View):
     def get(self, request, pk):
         todo = get_object_or_404(Todo, pk=pk)
-        #try:
-        send_mail('Emprestimo de livro',
-                'E-mail gerado automaticamente pelo sistema\nDevolução de livro atrasada\n', 
-                'bibliotecatasd@gmail.com',
-                [todo.cliente.email],)
-        todo.notified = True
-        todo.save()
-        #finally:
-        return redirect("todo_list")
+        try:
+            send_mail('Emprestimo de livro',
+                    'E-mail gerado automaticamente pelo sistema\nDevolução de livro atrasada\n', 
+                    'bibliotecatasd@gmail.com',
+                    [todo.cliente.email],)
+            todo.notified = True
+            todo.save()
+        finally:
+            return redirect("todo_list")
