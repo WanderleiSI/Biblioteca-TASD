1e5171a7d66812c512e55a1c00da6554316d3f5b
WanderleiSI <Wanderleirochavasco@hotmail.com>
2023-12-20 21:56:02 -0300

    Comentando config do e-mail

--- setup/settings.py
+++ setup/settings.py
@@ -135,10 +135,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 #Email
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+'''EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
-DEFAULT_FROM_EMAIL = "Biblioteca TASD | Aviso"
+DEFAULT_FROM_EMAIL = "Biblioteca TASD | Aviso"'''
