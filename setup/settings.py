c5f3bb9e1cbbe54643e737ede4d73c988c03279e
WanderleiSI <Wanderleirochavasco@hotmail.com>
2024-01-19 11:39:48 -0300

    Funcao excluir livro consertada

--- setup/settings.py
+++ setup/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = (
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 #Add do ip da instancia da AWS EC2, o padrão é a lista vazia
-ALLOWED_HOSTS = ["18.231.175.77"]
+ALLOWED_HOSTS = ["*"]
 
 DJANGO_APPS = [
     "django.contrib.admin",
